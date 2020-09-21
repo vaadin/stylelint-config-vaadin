5ea774efdcbc1ae838093131a23b10848bac84a9
web-padawan <iamkulykov@gmail.com>
2020-09-21 14:09:46 +0300

    Add text-size-adjust to allowed prefixes

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ module.exports = {
     "property-case": "lower",
     "property-no-unknown": true,
     "property-no-vendor-prefix": [true, {
-      "ignoreProperties": ["appearance", "mask-image", "user-select", "backdrop-filter"]
+      "ignoreProperties": ["appearance", "mask-image", "user-select", "backdrop-filter", "text-size-adjust"]
     }],
     "rule-empty-line-before": [ "always-multi-line", {
       except: ["first-nested"],
